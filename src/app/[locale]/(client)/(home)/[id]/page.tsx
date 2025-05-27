15e1f6706f63b2e3f251ff44c87b44705e30e1aa
Christ-Amour K <christ-amour@wazoom-studio.com>
2025-05-27 19:17:16 +0100

    fix : delete row-cols-xxl

--- src/app/[locale]/(client)/(home)/[id]/page.tsx
+++ src/app/[locale]/(client)/(home)/[id]/page.tsx
@@ -51,7 +51,7 @@ const Page: React.FC = () => {
 
 
   return (
-    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 row-cols-xxl-4 g-4">
+    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 g-4">
       <Suspense fallback={<BlockSkeleton image multiple className="col" count={10} />}>
         <LazyArticlesBlock
           articles={articles.data.filter(el => el.categoryId != null && el.categoryId != 5)} 
--- src/app/[locale]/(client)/(home)/page.tsx
+++ src/app/[locale]/(client)/(home)/page.tsx
@@ -44,7 +44,7 @@ const Page: React.FC = () => {
   };
 
   return (
-    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 row-cols-xxl-4 g-4">
+    <div className="row row-cols-1 row-cols-sm-2 row-cols-md-2 row-cols-lg-3 row-cols-xl-3 g-4">
       <Suspense fallback={<BlockSkeleton multiple image className="col" count={10} />}>
         <LazyArticlesBlock
           articles={articles.data.filter(el => el.categoryId != null && el.categoryId != 5)} 
